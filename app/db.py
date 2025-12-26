723e1d44f45002e47f374b3d9fcb91e35fe3b855
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 20:18:49 +0900

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,6 +1,5 @@
-# app/db.py
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./wms.db"
 
@@ -9,7 +8,10 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
