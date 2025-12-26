85c134514f902edecf686f9857c4c0cbde6264a0
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-27 00:22:25 +0900

    Add files via upload

--- PATCH_app_main.py
+++ PATCH_app_main.py
@@ -1,9 +1,5 @@
 
 # app/main.py 에 추가
 
-from app.pages import mobile, mobile_inbound, mobile_outbound, mobile_move
-
-app.include_router(mobile.router)
-app.include_router(mobile_inbound.router)
-app.include_router(mobile_outbound.router)
-app.include_router(mobile_move.router)
+from app.pages import admin
+app.include_router(admin.router)
