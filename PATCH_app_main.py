cb8a289f08ffcac3cd974b8ad6125703d91d15e8
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-27 00:20:37 +0900

    Add files via upload

--- PATCH_app_main.py
+++ PATCH_app_main.py
@@ -1,8 +1,9 @@
 
 # app/main.py 에 추가
 
-from app.routers import calendar as calendar_router
-from app.pages import calendar as calendar_page
+from app.pages import mobile, mobile_inbound, mobile_outbound, mobile_move
 
-app.include_router(calendar_router.router)
-app.include_router(calendar_page.router)
+app.include_router(mobile.router)
+app.include_router(mobile_inbound.router)
+app.include_router(mobile_outbound.router)
+app.include_router(mobile_move.router)
