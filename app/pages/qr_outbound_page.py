81aeb41ce4c63549498c08b34e4e9b4040bd7c46
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 15:54:15 +0900

    Delete app/pages/qr_outbound_page.py

--- app/pages/qr_outbound_page.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-from ._common import templates
-
-router = APIRouter(prefix="/qr", tags=["qr"])
-
-@router.get("/outbound")
-def page(request: Request):
-    return templates.TemplateResponse("qr_outbound.html", {"request": request})
