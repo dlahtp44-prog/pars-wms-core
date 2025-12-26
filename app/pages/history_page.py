bb1f8eb173716a3560fcda134d57b75271abddd4
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 15:52:36 +0900

    Delete app/pages/history_page.py

--- app/pages/history_page.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-from ._common import templates
-
-router = APIRouter()
-
-@router.get("/history")
-def page(request: Request):
-    return templates.TemplateResponse("history.html", {"request": request})
