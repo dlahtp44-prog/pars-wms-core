feccffd08940a1f01dbb52178bc936d84581e794
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 15:52:46 +0900

    Delete app/pages/inbound_page.py

--- app/pages/inbound_page.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-from ._common import templates
-
-router = APIRouter()
-
-@router.get("/inbound")
-def page(request: Request):
-    return templates.TemplateResponse("inbound.html", {"request": request})
