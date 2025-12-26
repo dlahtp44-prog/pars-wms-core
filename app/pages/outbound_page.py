2e85bcf49c4b8f1ed3d258b6a09d7aea10bc26e0
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 15:53:47 +0900

    Delete app/pages/outbound_page.py

--- app/pages/outbound_page.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-from ._common import templates
-
-router = APIRouter()
-
-@router.get("/outbound")
-def page(request: Request):
-    return templates.TemplateResponse("outbound.html", {"request": request})
