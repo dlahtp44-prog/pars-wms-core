37367ed074c18c0725b01502dcef6e4f0a75a658
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-26 15:34:03 +0900

    Create inbound.py

---
+++ app/pages/inbound.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
+
+
+@router.get("/입고", response_class=HTMLResponse)
+def page(request: Request):
+    return templates.TemplateResponse("inbound.html", {"request": request})
