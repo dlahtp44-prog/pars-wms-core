a5d7256bfbfa7406f47c42ae0e7c40f97fe6ca21
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-27 10:03:26 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -4,19 +4,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title="PARS WMS CORE UI")
+app = FastAPI(title="PARS WMS")
 
 templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-def root(request: Request):
-    return templates.TemplateResponse("mobile_home.html", {"request": request})
-
-@app.get("/m/calendar", response_class=HTMLResponse)
-def calendar(request: Request):
-    return templates.TemplateResponse("mobile_calendar.html", {"request": request})
-
-@app.get("/m/qr", response_class=HTMLResponse)
-def qr(request: Request):
-    return templates.TemplateResponse("mobile_qr.html", {"request": request})
+def mobile_home(request: Request):
+    return templates.TemplateResponse("mobile_menu.html", {"request": request})
