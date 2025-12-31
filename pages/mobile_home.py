54317cd48b676a0295de829e3296cdfcf8ef5607
dlahtp44-prog <dlahtp44@gmail.com>
2025-12-31 22:15:50 +0900

    Add files via upload

--- pages/mobile_home.py
+++ pages/mobile_home.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Request
+
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
-templates = Jinja2Templates(directory='app/templates')
+router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
-router = APIRouter(prefix="/m")
-
-@router.get("")
+@router.get("/m", response_class=HTMLResponse)
 def mobile_home(request: Request):
-    return templates.TemplateResponse("mobile_home.html", {"request": request})
+    return templates.TemplateResponse("m/index.html", {"request": request})
