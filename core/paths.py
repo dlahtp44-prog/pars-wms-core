cd4af1536342d3a6e08f63ece66a99545c825111
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-01 22:25:07 +0900

    Delete core directory

--- core/paths.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent.parent  # project root
-APP_DIR = BASE_DIR / "app"
-TEMPLATES_DIR = APP_DIR / "templates"
-STATIC_DIR = APP_DIR / "static"
