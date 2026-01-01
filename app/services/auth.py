bac7faf155cf8afac6a3280a91bc92eb6fda2246
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-01 22:24:47 +0900

    Delete app directory

--- app/services/auth.py
+++
@@ -1,7 +0,0 @@
-
-from fastapi import Request
-from fastapi.responses import RedirectResponse
-
-def require_admin(request: Request):
-    if request.session.get("role") != "admin":
-        return RedirectResponse("/login", status_code=302)
