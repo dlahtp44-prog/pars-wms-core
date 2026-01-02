d99f8ccddd8587bf411d4dbe56353e10fee09ffa
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-02 09:08:23 +0900

    Delete pages directory

--- pages/qr_page.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
-
-router = APIRouter(prefix="/m")
-templates = Jinja2Templates(directory="app/templates")
-
-@router.get("/qr")
-def qr(request: Request):
-    return templates.TemplateResponse("qr.html", {"request": request, "title": "QR 스캔"})
