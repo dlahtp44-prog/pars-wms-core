adf829d83bd9ab80fa40ee4932f6cfdc2c9a2a87
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-02 09:48:07 +0900

    Delete history.py

--- history.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-router = APIRouter()
-templates = Jinja2Templates(directory="app/templates")
-@router.get("/이력", response_class=HTMLResponse)
-def page(request: Request):
-    return templates.TemplateResponse("history.html", {"request": request})
